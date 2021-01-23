0139bffbeef6f3eb3b6b13d5331faf31f06e9625
Евгения <april-may@mail.ru>
2021-01-23 12:09:03 +0300

    Редактирование

--- gulpfile.js
+++ gulpfile.js
@@ -13,10 +13,44 @@ const webp = require("gulp-webp");
 const svgstore = require("gulp-svgstore");
 const del = require("del");
 const sync = require("browser-sync").create();
+// const cheerio = require("gulp-cheerio");
 
 // Styles
 
+// const styles = () => {
+//   return gulp.src("source/less/style.less")
+//     .pipe(plumber())
+//     .pipe(sourcemap.init())
+//     .pipe(less())
+//     .pipe(postcss([
+//       autoprefixer(),
+//       csso()
+//     ]))
+//     .pipe(rename("style.min.css"))
+//     .pipe(sourcemap.write("."))
+//     .pipe(gulp.dest("build/css"))
+//     .pipe(sync.stream());
+// }
+
+// exports.styles = styles;
+
 const styles = () => {
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(rename("style.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(sync.stream());
+}
+
+exports.styles = styles;
+
+const stylesmin = () => {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
     .pipe(sourcemap.init())
@@ -31,7 +65,7 @@ const styles = () => {
     .pipe(sync.stream());
 }
 
-exports.styles = styles;
+exports.stylesmin = stylesmin;
 
 // HTML
 
@@ -40,6 +74,18 @@ const html = () => {
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("build"));
 }
+
+// Scripts
+
+const scripts = () => {
+  return gulp.src("source/js/script.js")
+    .pipe(uglify())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(sync.stream());
+}
+
+exports.scripts = scripts;
 
 // Images
 
@@ -64,6 +110,25 @@ const createWebp = () => {
 }
 
 exports.createWebp = createWebp;
+
+// Sprite
+
+const sprite = () => {
+  return gulp.src("source/img/icons/*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"))
+    // .pipe(cheerio({
+    //   run: function ($) {
+    //     $('[fill]').removeAttr('fill');
+    //     $('[stroke]').removeAttr('stroke');
+    //     $('[style]').removeAttr('style');
+    //   },
+    //   parserOptions: {xmlMode: true}
+    // }))
+}
+
+exports.sprite = sprite;
 
 // Copy
 
@@ -113,7 +178,9 @@ const reload = done => {
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series(styles));
+  // gulp.watch("source/less/**/*.less", gulp.series(styles));
+  gulp.watch("source/less/**", gulp.series(build, reload));
+  gulp.watch("source/js/script.js", gulp.series(scripts));
   gulp.watch("source/*.html", gulp.series(html, reload));
 }
 
@@ -123,7 +190,10 @@ const build = gulp.series(
   clean,
   gulp.parallel(
     styles,
+    stylesmin,
     html,
+    scripts,
+    sprite,
     copy,
     images,
     createWebp
@@ -137,7 +207,10 @@ exports.default = gulp.series(
   clean,
   gulp.parallel(
     styles,
+    stylesmin,
     html,
+    scripts,
+    sprite,
     copy,
     createWebp
   ),
@@ -145,3 +218,153 @@ exports.default = gulp.series(
     server,
     watcher
   ));
+
+
+
+// const gulp = require("gulp");
+// const plumber = require("gulp-plumber");
+// const sourcemap = require("gulp-sourcemaps");
+// const less = require("gulp-less");
+// const postcss = require("gulp-postcss");
+// const autoprefixer = require("autoprefixer");
+// const csso = require("postcss-csso");
+// const rename = require("gulp-rename");
+// const htmlmin = require("gulp-htmlmin");
+// const uglify = require("gulp-uglify");
+// const imagemin = require("gulp-imagemin");
+// const webp = require("gulp-webp");
+// const svgstore = require("gulp-svgstore");
+// const del = require("del");
+// const sync = require("browser-sync").create();
+
+// // Styles
+
+// const styles = () => {
+//   return gulp.src("source/less/style.less")
+//     .pipe(plumber())
+//     .pipe(sourcemap.init())
+//     .pipe(less())
+//     .pipe(postcss([
+//       autoprefixer(),
+//       csso()
+//     ]))
+//     .pipe(rename("style.min.css"))
+//     .pipe(sourcemap.write("."))
+//     .pipe(gulp.dest("build/css"))
+//     .pipe(sync.stream());
+// }
+
+// exports.styles = styles;
+
+// // HTML
+
+// const html = () => {
+//   return gulp.src("source/*.html")
+//     .pipe(htmlmin({ collapseWhitespace: true }))
+//     .pipe(gulp.dest("build"));
+// }
+
+// // Images
+
+// const images = () => {
+//   return gulp.src("source/img/**/*.{png,jpg,svg}")
+//     .pipe(imagemin([
+//       imagemin.mozjpeg({progressive: true}),
+//       imagemin.optipng({optimizationLevel: 3}),
+//       imagemin.svgo()
+//     ]))
+//     .pipe(gulp.dest("build/img"))
+// }
+
+// exports.images = images;
+
+// // WebP
+
+// const createWebp = () => {
+//   return gulp.src("source/img/**/*.{jpg,png}")
+//     .pipe(webp({quality: 90}))
+//     .pipe(gulp.dest("build/img"))
+// }
+
+// exports.createWebp = createWebp;
+
+// // Copy
+
+// const copy = (done) => {
+//   gulp.src([
+//     "source/fonts/*.{woff2,woff}",
+//     "source/*.ico",
+//     "source/img/**/*.{jpg,png,svg}",
+//   ], {
+//     base: "source"
+//   })
+//     .pipe(gulp.dest("build"))
+//   done();
+// }
+
+// exports.copy = copy;
+
+// // Clean
+
+// const clean = () => {
+//   return del("build");
+// };
+
+// // Server
+
+// const server = (done) => {
+//   sync.init({
+//     server: {
+//       baseDir: "build"
+//     },
+//     cors: true,
+//     notify: false,
+//     ui: false,
+//   });
+//   done();
+// }
+
+// exports.server = server;
+
+// // Reload
+
+// const reload = done => {
+//   sync.reload();
+//   done();
+// }
+
+// // Watcher
+
+// const watcher = () => {
+//   gulp.watch("source/less/**/*.less", gulp.series(styles));
+//   gulp.watch("source/*.html", gulp.series(html, reload));
+// }
+
+// // Build
+
+// const build = gulp.series(
+//   clean,
+//   gulp.parallel(
+//     styles,
+//     html,
+//     copy,
+//     images,
+//     createWebp
+//   ));
+
+// exports.build = build;
+
+// // Default
+
+// exports.default = gulp.series(
+//   clean,
+//   gulp.parallel(
+//     styles,
+//     html,
+//     copy,
+//     createWebp
+//   ),
+//   gulp.series(
+//     server,
+//     watcher
+//   ));
